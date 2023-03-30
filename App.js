c809338b4a01cbe8a69fd14d6b82f95ee8ef9499
Yogapriyavadivel <112624031+Yogapriyavadivel@users.noreply.github.com>
2023-03-30 10:45:33 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Circle from './Circle';
-import Button from './Button';
 
-function App(){
-  return(
-    <Button/>
+import './App.css';
+import Comp1 from './Comp1';
+import Comp2 from './Comp2';
+import Comp3 from './Comp3';
+import Comp0 from './Comp0';
+
+function App() {
+  return (
+    <div className="App">
+      <Comp0/>
+      <br/>
+      <Comp1/>
+      <Comp3/>
+      <Comp2/>
+    </div>
   );
 }
+
 export default App;
---
+++ Comp2.js
@@ -0,0 +1,11 @@
+function Comp2(){
+    return(
+        <>
+        <h3>Audi M3</h3>
+        <p>Recently it also added two new variants in its popular M series.<br/> Among those two, Audi M Series M3 Sedan is the four door variant, which is powered by a<br/> 3.0-litre petrol engine under the hood.<br/> The manufacturer claims that the vehicle can break the 100 Kmph speed</p>
+        <button>Buy Now</button>
+        </>
+     );
+}
+
+export default Comp2;
